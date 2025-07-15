f0f5bdbeb468f2d47a40d7fd7f18422398672646
Infohubsite <gigwadorusdominante@gmail.com>
2025-07-15 23:06:52 +0300

    added a way to troll your teammates by changing their oxygen (host only) :D

---
+++ Features/Commands/CommandsFeature.cs
@@ -0,0 +1,21 @@
+﻿namespace Tweaks.Features.Commands
+{
+    [ModFeature]
+    internal class CommandsFeature : Feature
+    {
+        public static CommandsFeature Instance { get; set; } = null!;
+
+        internal override string FeatureName => "Commands";
+        protected override string FeatureDescription => "Adds various console commands.";
+
+        public CommandsFeature()
+        {
+            Instance = this;
+        }
+
+        public override void Initialize()
+        {
+            PlayerPatch.Init();
+        }
+    }
+}
---
+++ Features/Commands/PlayerPatch.cs
@@ -0,0 +1,18 @@
+﻿namespace Tweaks.Features.Commands
+{
+    internal class PlayerPatch
+    {
+        internal static void Init()
+        {
+            On.Player.Start += Player_Start;
+        }
+
+        private static System.Collections.IEnumerator Player_Start(On.Player.orig_Start orig, Player self)
+        {
+            yield return orig(self);
+
+            if (self.gameObject.GetComponent<PlayerNetworkHandler>() == null)
+                self.gameObject.AddComponent<PlayerNetworkHandler>();
+        }
+    }
+}
